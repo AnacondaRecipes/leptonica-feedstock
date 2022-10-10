550f02bddaf4655a39623e8322d2331a99e1f90b
Serhii Kupriienko <79282962+skupr-anaconda@users.noreply.github.com>
2022-10-10 14:01:48 +0300

    Add leptonica 1.82.0 (#1)
    * Add license_url
    
    * Fix overdepending errors on win64 and osx-arm64
    
    * Disable sw package manager
    
    * giflib not on win
    
    * Use ninja
    
    * Add libwebp-base to ignore_run_exports for osx-arm64 & win
    
    * Remove xz

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,33 +1,42 @@
-mkdir build
-cd build
-
-curl -fsSOL https://software-network.org/client/sw-master-windows-client.zip
-if errorlevel 1 exit 1
-
-unzip sw-master-windows-client.zip -d .
-if errorlevel 1 exit 1
-
+:: cmd
 set PATH=%PATH%;%CD%
 
-sw setup
-if errorlevel 1 exit 1
+echo "Building %PKG_NAME%."
 
-cmake -G "NMake Makefiles" ^
-      -D CMAKE_BUILD_TYPE=Release ^
-      -D BUILD_PROG=1 ^
-      -D CMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
-      -D CMAKE_INCLUDE_PATH=%LIBRARY_INC% ^
-      -D CMAKE_LIBRARY_PATH=%LIBRARY_LIB% ^
-      -D CMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+
+:: Isolate the build.
+mkdir Build-%PKG_NAME%
+cd Build-%PKG_NAME%
+if errorlevel 1 exit /b 1
+
+:: Generate the build files.
+echo "Generating the build files..."
+cmake .. %CMAKE_ARGS% ^
+      -G"Ninja" ^
+      -DCMAKE_BUILD_TYPE=Release ^
+      -DBUILD_PROG=1 ^
+      -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
+      -DCMAKE_INCLUDE_PATH=%LIBRARY_INC% ^
+      -DCMAKE_LIBRARY_PATH=%LIBRARY_LIB% ^
+      -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
+      -DSW_BUILD=OFF ^
       ..
 if errorlevel 1 exit 1
 
-cmake --build . --config Release
-if errorlevel 1 exit 1
+:: Build.
+echo "Building..."
+ninja
+if errorlevel 1 exit /b 1
 
-cmake --build . --config Release --target install
-if errorlevel 1 exit 1
+:: Install.
+echo "Installing..."
+ninja install
+if errorlevel 1 exit /b 1
 
 :: Make copies of the .lib file without the embedded version number
 copy %LIBRARY_LIB%\leptonica-1.82.0.lib %LIBRARY_LIB%\leptonica.lib
 copy %LIBRARY_LIB%\leptonica-1.82.0.lib %LIBRARY_LIB%\lept.lib
+
+:: Error free exit.
+echo "Error free exit!"
+exit 0
